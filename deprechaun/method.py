630cc7a65b83df7977057d84453f1c961a667d88
MathSquared <MathSquared@users.noreply.github.com>
2023-12-09 17:07:43 -0600

    Add SL depreciation method

---
+++ deprechaun/method.py
@@ -0,0 +1,24 @@
+from decimal import Decimal
+from typing import Protocol, Tuple
+
+from deprechaun.asset import Asset
+
+
+ONE: Decimal = Decimal('1')
+ZERO: Decimal = Decimal('0')
+
+
+class DepreciationMethod(Protocol):
+    def __call__(self, asset: Asset, period: Decimal = ...) -> Tuple[Decimal, Asset]: ...
+
+
+def straight_line(asset: Asset, period: Decimal = ONE) -> Tuple[Decimal, Asset]:
+    if period >= asset.life:
+        dep = asset.basis
+        ret = Asset(basis=ZERO, life=ZERO, method=asset.method)
+    else:
+        dep = asset.basis / asset.life
+        rbasis = asset.basis - dep
+        rlife = asset.life - period
+        ret = Asset(basis=rbasis, life=rlife, method=asset.method)
+    return ret
