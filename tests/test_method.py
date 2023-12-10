607fd3f1a4d753630f426b5feb06244c7b1446e6
MathSquared <MathSquared@users.noreply.github.com>
2023-12-09 18:57:00 -0600

    Add other tests for straight-line depreciation

--- tests/test_method.py
+++ tests/test_method.py
@@ -23,3 +23,36 @@ class TestStraightLine(unittest.TestCase):
         for i in range(6):
             res.append(straight_line(res[-1][1]))
         self.assertEqual(expect, res)
+
+    def test_fractional_year(self):
+        res = [(None, Asset(Decimal('3'), Decimal('1.5'), straight_line))]
+
+        expect = [
+            (None, Asset(Decimal('3'), Decimal('1.5'), straight_line)),
+            (Decimal('2'), Asset(Decimal('1'), Decimal('0.5'), straight_line)),
+            (Decimal('1'), Asset(Decimal('0'), Decimal('0'), straight_line)),
+            (Decimal('0'), Asset(Decimal('0'), Decimal('0'), straight_line)),
+        ]
+
+        for i in range(3):
+            res.append(straight_line(res[-1][1]))
+        self.assertEqual(expect, res)
+
+    def test_fractional_period(self):
+        a = Asset(Decimal('4'), Decimal('2'), straight_line)
+
+        dep, a = straight_line(a, period=Decimal('0.5'))
+        self.assertEqual(Decimal('1'), dep)
+        self.assertEqual(Asset(Decimal('3'), Decimal('1.5'), straight_line), a)
+
+        dep, a = straight_line(a)
+        self.assertEqual(Decimal('2'), dep)
+        self.assertEqual(Asset(Decimal('1'), Decimal('0.5'), straight_line), a)
+
+        dep, a = straight_line(a)
+        self.assertEqual(Decimal('1'), dep)
+        self.assertEqual(Asset(Decimal('0'), Decimal('0'), straight_line), a)
+
+        dep, a = straight_line(a)
+        self.assertEqual(Decimal('0'), dep)
+        self.assertEqual(Asset(Decimal('0'), Decimal('0'), straight_line), a)
