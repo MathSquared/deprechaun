89a555bef0a3586918c2e402c7a656e7400dbe5a
MathSquared <MathSquared@users.noreply.github.com>
2023-12-10 15:11:46 -0600

    Add declining_balance_macrs

--- deprechaun/method.py
+++ deprechaun/method.py
@@ -43,7 +43,7 @@ def declining_balance_only(rate: int | Decimal) -> DepreciationMethod:
 
     See IRS, "How to Depreciate Property," Publication 946 (2022), 38-39. Note that salvage value is not supported.
 
-    If you are using MACRS, this is probably not what you're looking for, since MACRS requires switching to the straight-line method when it returns an equal or greater deduction.
+    If you are using MACRS, this is probably not what you're looking for, since MACRS requires switching to the straight-line method when it returns an equal or greater deduction. Instead, use ``declining_balance_macrs``.
 
     Args:
         rate (int | Decimal): The declining-balance rate, **in percent** (so call ``declining_balance_only(40)`` for 40%). This is the declining-balance percentage **divided by the useful life of the property**.
@@ -59,3 +59,26 @@ def declining_balance_only(rate: int | Decimal) -> DepreciationMethod:
             return asset.basis * rate / 100 * min(period, asset.life)
         _DECLINING_BALANCE_ONLY_MEMO[rate] = declining_balance_only_impl
         return declining_balance_only_impl
+
+
+_DECLINING_BALANCE_MACRS_MEMO: dict[Decimal, DepreciationMethod] = {}
+
+def declining_balance_macrs(rate: int | Decimal) -> DepreciationMethod:
+    """Returns a function for the declining-balance depreciation method, switching to straight-line when it produces an equal or greater deduction.
+
+    See IRS, "How to Depreciate Property," Publication 946 (2022), 38-39. Note that salvage value is not supported.
+
+    Args:
+        rate (int | Decimal): The declining-balance rate, **in percent** (so call ``declining_balance_macrs(40)`` for 40%). This is the declining-balance percentage **divided by the recovery period of the property**.
+
+    Returns:
+        DepreciationMethod: A function implementing the declining-balance MACRS depreciation method for the given rate. This will be the same function for the same rate.
+    """
+    rate = Decimal(rate)
+    if rate in _DECLINING_BALANCE_MACRS_MEMO:
+        return _DECLINING_BALANCE_MACRS_MEMO[rate]
+    else:
+        def declining_balance_macrs_impl(asset: Asset, period: Decimal = ONE) -> Decimal:
+            return max(straight_line(asset, period), declining_balance_only(rate)(asset, period))
+        _DECLINING_BALANCE_MACRS_MEMO[rate] = declining_balance_macrs_impl
+        return declining_balance_macrs_impl
--- tests/test_method.py
+++ tests/test_method.py
@@ -3,7 +3,7 @@ import unittest
 from decimal import Decimal
 
 from deprechaun.asset import Asset
-from deprechaun.method import declining_balance_only, straight_line
+from deprechaun.method import declining_balance_macrs, declining_balance_only, straight_line
 
 
 class TestStraightLine(unittest.TestCase):
@@ -40,3 +40,25 @@ class TestDecliningBalanceOnly(unittest.TestCase):
         # Declining-balance is asymptotic and will not recover the whole value
         # (that's why MACRS switches to straight-line)
         self.assertEqual(Decimal('0.2'), declining_balance_only(40)(Asset(Decimal('1'), Decimal('0.5'), declining_balance_only(40))))
+
+
+class TestDecliningBalanceMacrs(unittest.TestCase):
+    def test_memo(self):
+        self.assertEqual(declining_balance_macrs(200), declining_balance_macrs(200))
+        self.assertEqual(declining_balance_macrs(200), declining_balance_macrs(Decimal('200')))
+        self.assertNotEqual(declining_balance_macrs(200), declining_balance_macrs(150))
+
+    def test_hy_5(self):
+        # See IRS, "How to Depreciate Property," Publication 946 (2022), 69, table A-1.
+        a = Asset(Decimal('100'), Decimal('5'), declining_balance_macrs(40), -2)
+        deps = []
+        
+        dep, a = a.depreciate(period=Decimal('0.5'))
+        deps.append(dep)
+        for _ in range(6):  # one more off the end
+            dep, a = a.depreciate()
+            deps.append(dep)
+
+        self.assertEqual(deps, [
+            Decimal('20'), Decimal('32'), Decimal('19.2'), Decimal('11.52'), Decimal('11.52'), Decimal('5.76'), Decimal('0'),
+        ])
