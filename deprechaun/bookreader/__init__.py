fc344620bf8c5522b3b1d7c3fc41be22da713468
MathSquared <MathSquared@users.noreply.github.com>
2023-12-10 17:19:29 -0600

    Add bookreader module

---
+++ deprechaun/bookreader/__init__.py
@@ -0,0 +1,8 @@
+"""Code for converting accounting records into depreciable assets.
+
+This code takes in asset information extracted from an accounting record, and is responsible for:
+
+- checking conditions imposed by the Internal Revenue Code;
+- making certain selections, such as a depreciation convention, that are made based on the aggregate of depreciable assets; and
+- producing an ``Asset`` object for each depreciable asset.
+"""
