aef821b51d3c4e34b8229f8d836145f61b61de29
MathSquared <MathSquared@users.noreply.github.com>
2023-12-09 19:14:52 -0600

    Add docstrings

--- deprechaun/asset.py
+++ deprechaun/asset.py
@@ -8,6 +8,17 @@ if TYPE_CHECKING:
 
 
 class Asset(NamedTuple):
+    """A depreciable asset.
+
+    This class includes all information about an asset necessary to depreciate it.
+
+    It deliberately does not include the name of the asset, date it was placed in service, or similar information that is used to identify the asset but is not relevant to depreciation. It also does not include when in the year the asset was placed in service, the convention that is used, or similar. The amount that remains to be recovered in the last year of depreciation should be represented as the fractional part of the life parameter.
+
+    Attributes:
+        basis (Decimal): The adjusted basis of the asset for depreciation, less any depreciation that has already been deducted.
+        life (Decimal): The remaining useful life or recovery period of the asset, in years.
+        method (DepreciationMethod): The default depreciation method for the asset.
+    """
     basis: Decimal
     life: Decimal
     method: 'DepreciationMethod'
--- deprechaun/method.py
+++ deprechaun/method.py
@@ -9,10 +9,27 @@ ZERO: Decimal = Decimal('0')
 
 
 class DepreciationMethod(Protocol):
+    """Depreciates an asset over a given period.
+
+    Functions with this protocol implement various depreciation methods. The interface for all such methods is given here.
+
+    These functions will not depreciate an asset beyond its useful life. If the period is greater than the asset's remaining life, the entire basis will be returned as the depreciation value, and the returned asset will have its basis and life both set to zero.
+
+    Args:
+        asset (Asset): The asset to depreciate.
+        period (Decimal): The period over which to depreciate the asset. For example, for the year the asset was placed in service, this could reflect the time during the year that it was placed in service, or the convention used for depreciation.
+
+    Returns:
+        Tuple[Decimal, Asset]: The amount of depreciation, and a new asset with the basis reduced by the depreciation amount, the life reduced by the period, and the same method as the original asset.
+    """
     def __call__(self, asset: Asset, period: Decimal = ...) -> Tuple[Decimal, Asset]: ...
 
 
 def straight_line(asset: Asset, period: Decimal = ONE) -> Tuple[Decimal, Asset]:
+    """The straight-line depreciation method.
+
+    See IRS, "How to Depreciate Property," Publication 946 (2022), 39. Note that salvage value is not supported.
+    """
     if period >= asset.life:
         dep = asset.basis
         ret = Asset(basis=ZERO, life=ZERO, method=asset.method)
