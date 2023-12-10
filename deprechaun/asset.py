15b8098d0387a4f3adc4763a0cd13c2da3f5e372
MathSquared <MathSquared@users.noreply.github.com>
2023-12-09 19:15:15 -0600

    Remove unused imports from asset.py

--- deprechaun/asset.py
+++ deprechaun/asset.py
@@ -1,7 +1,5 @@
-from __future__ import annotations
-
 from decimal import Decimal
-from typing import Callable, NamedTuple, Tuple, TYPE_CHECKING
+from typing import NamedTuple, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from deprechaun.method import DepreciationMethod
